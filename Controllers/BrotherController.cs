0ea34dd3cb632b085728fb69b25eb5d59c0b3840
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2021-12-28 18:19:35 +0100

    Added controller with first action fetching brothers

---
+++ Controllers/BrotherController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntityFrameworkLearn.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkLearn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrotherController : ControllerBase
+    {
+        private readonly BrothersDbContext _context;
+        public BrotherController(BrothersDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Brother>>> GetBrothers()
+        {
+            return await _context.Brothers.ToListAsync();
+        }
+    }
+}
